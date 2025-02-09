42f1288aecc0851db29bc0cb1bd932925ed87635
Roger Faria <Roger.Faria.Dev@outlook.com>
2025-02-09 13:40:40 -0300

    refactor: changes in the tostify provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Tasks } from './components/Tasks/Tasks';
 import { SideBar } from './components/SideBar/SideBar';
 import './App.scss';
-import { ToastContainer } from 'react-toastify';
 
 function App() {
 	return (
 		<div className="app-container">
 			<SideBar />
 			<Tasks />
-			<ToastContainer />
 		</div>
 	);
 }
