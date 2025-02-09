51c5e55aaadf58c26d8f2322b249d0673a9a8f76
Roger Faria <Roger.Faria.Dev@outlook.com>
2025-02-09 14:30:17 -0300

    feat: add navigation routes to the custom button

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,15 +1,19 @@
 import { CustomButton } from '../CustomButton/CustomButton';
 import logo from '../../assets/navLogo.png';
 import './SideBar.scss';
+import { useNavigate } from 'react-router-dom';
 
 const SideBar = () => {
+	const navigate = useNavigate();
+	const handleSignOut = () => navigate('/login');
+
 	return (
 		<div className="sidebar-container">
 			<div className="logo">
 				<img src={logo} alt="Full Stack Club" />
 			</div>
 			<div className="sign-out">
-				<CustomButton>Sair</CustomButton>
+				<CustomButton onClick={handleSignOut}>Sair</CustomButton>
 			</div>
 		</div>
 	);
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo-2.png';
 import { CustomButton } from '../../components/CustomButton';
 import './Login.scss';
 
 const Login = () => {
+	const navigate = useNavigate();
+
+	const handleSignInClick = () => {
+		navigate('/');
+	};
+
 	return (
 		<div className="login-container">
 			<img src={logo} alt="Full Stack Club" />
-			<CustomButton>Sign in</CustomButton>
+			<CustomButton onClick={handleSignInClick}>Sign in</CustomButton>
 		</div>
 	);
 };
